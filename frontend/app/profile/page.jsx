8bf8559d9dd7ca4870a60c33f6a3f9ed5456b492
Jeddx <4jedijed@gmail.com>
2025-03-22 18:53:01 -0400

    Added dialog opening button, todo: format the fields and post to database

--- frontend/app/profile/page.jsx
+++ frontend/app/profile/page.jsx
@@ -1,40 +1,37 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import { ChartAreaInteractive } from "@/components/chart-area-interactive"
-import { DataTable } from "@/components/data-table"
-import { SectionCards } from "@/components/section-cards"
-import { SiteHeader } from "@/components/site-header"
-import {
-  SidebarInset,
-  SidebarProvider,
-} from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar";
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { DataTable } from "@/components/data-table";
+import { SectionCards } from "@/components/section-cards";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-import data from "../dashboard/data.json"
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
-import { Card } from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
-import { Button } from "@/components/ui/button"
-import ProfileTab from "@/components/profile-tab"
-import ProfileList from "@/components/profile-list"
+import data from "../dashboard/data.json";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Card } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import ProfileTab from "@/components/profile-tab";
+import ProfileList from "@/components/profile-list";
+import ProfileEdit from "@/components/profile-edit";
 
 export default function Page() {
   const cards = [
     {
       title: "Roommates",
-      value: "YESSIR"
+      value: "YESSIR",
     },
     {
       title: "Dorm",
-      value: "YESSIR"
-    }
+      value: "YESSIR",
+    },
   ];
   return (
     <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)"
-        }
-      }>
+      style={{
+        "--sidebar-width": "calc(var(--spacing) * 72)",
+        "--header-height": "calc(var(--spacing) * 12)",
+      }}
+    >
       <AppSidebar variant="inset" />
       <SidebarInset>
         <SiteHeader>Profile</SiteHeader>
@@ -51,6 +48,7 @@ export default function Page() {
               <ProfileList></ProfileList>
               <div className="flex justify-center px-4 lg:px-6">
                 <Button>Edit</Button>
+                <ProfileEdit></ProfileEdit>
               </div>
             </div>
           </div>
