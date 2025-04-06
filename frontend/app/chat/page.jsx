f4f9f23ef39a97b3ee978f5e75a34efe73c87982
Jeddx <4jedijed@gmail.com>
2025-04-06 18:18:27 -0400

    Added basic chatbox component and made directory for chat routing

---
+++ frontend/app/chat/page.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SectionCards } from "@/components/section-cards";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+
+export default function Page() {
+  const cards = [
+    {
+      title: "Roommate",
+      value: "John Smith",
+    },
+    {
+      title: "Dorm",
+      value: "Thomas Suites",
+    },
+  ];
+  return (
+    <SidebarProvider
+      style={{
+        "--sidebar-width": "calc(var(--spacing) * 72)",
+        "--header-height": "calc(var(--spacing) * 12)",
+      }}
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader>Chat</SiteHeader>
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards cards={cards} width={2} />
+              <div className="px-4 lg:px-6"></div>
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
